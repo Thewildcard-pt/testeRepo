b13f2c36ea38e4a2ced7ee53e777307b8ef5a999
Thewildcard <31257166+Thewildcard-pt@users.noreply.github.com>
2018-09-12 22:42:22 +0100

    welp lets do this

---
+++ app/src/main/java/ipca/edjd/world/MainActivity.java
@@ -0,0 +1,41 @@
+package ipca.edjd.world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final TextView textv = new TextView(this);
+        textv.setText("hello there");
+
+
+        Button btn = new Button(this);
+        btn.setText("Traduzir");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                textv.setText("olá mundo: "+ count);
+            }
+        });
+
+        LinearLayout lines = new LinearLayout(this);
+        lines.addView(textv);
+        lines.addView(btn);
+
+        setContentView(lines);
+    }
+}
